eecc04d15ae01b2503590da9f5e01a33fd0270a8
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2025-09-19 12:31:54 +0530

    add GET and SET method; add resp writer

--- app/main.go
+++ app/main.go
@@ -12,22 +12,90 @@ import (
 const (
 	ECHO = "ECHO"
 	PING = "PING"
+	GET  = "GET"
+	SET  = "SET"
+)
+
+type respStringType string
+
+const (
+	BULK   respStringType = "BULK"
+	SIMPLE respStringType = "SIMPLE"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
+type Store struct {
+	data map[string]string
+}
+
+func NewStore() *Store {
+	return &Store{
+		data: make(map[string]string),
+	}
+}
+
+func (s *Store) Set(key, value string) {
+	s.data[key] = value
+}
+
+func (s *Store) Get(key string) (string, bool) {
+	val, ok := s.data[key]
+	return val, ok
+}
+
+var GlobalStore = NewStore()
+
+func handleGet(conn net.Conn, key string) error {
+	val, ok := GlobalStore.Get(key)
+	if ok {
+		respStr, err := respWriter(BULK, val)
+		if err != nil {
+			return err
+		}
+		if _, err := conn.Write([]byte(respStr)); err != nil {
+			return err
+		}
+		return nil
+	} else {
+		if _, err := conn.Write([]byte("$-1\r\n")); err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func handleSet(conn net.Conn, key, value string) error {
+	GlobalStore.Set(key, value)
+	respStr, err := respWriter(SIMPLE, "OK")
+	if err != nil {
+		return err
+	}
+	if _, err := conn.Write([]byte(respStr)); err != nil {
+		return err
+	}
+	return nil
+}
+
 func handleEcho(conn net.Conn, str string) error {
-	msg := fmt.Sprintf("$%d\r\n%s\r\n", len(str), str)
-	if _, err := conn.Write([]byte(msg)); err != nil {
+	respStr, err := respWriter(BULK, str)
+	if err != nil {
+		return err
+	}
+	if _, err := conn.Write([]byte(respStr)); err != nil {
 		return err
 	}
 	return nil
 }
 
 func handlePing(conn net.Conn) error {
-	if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
+	respStr, err := respWriter(SIMPLE, "PONG")
+	if err != nil {
+		return err
+	}
+	if _, err := conn.Write([]byte(respStr)); err != nil {
 		return err
 	}
 	return nil
@@ -46,6 +114,16 @@ func respParser(conn net.Conn) ([]string, error) {
 		args = append(args, string(parts[i]))
 	}
 	return args, nil
+}
+
+func respWriter(strType respStringType, str string) (string, error) {
+	switch strType {
+	case BULK:
+		return fmt.Sprintf("$%d\r\n%s\r\n", len(str), str), nil
+	case SIMPLE:
+		return fmt.Sprintf("+%s\r\n", str), nil
+	}
+	return "", fmt.Errorf("unknown response type: %s", strType)
 }
 
 func handleConnection(conn net.Conn) error {
@@ -68,6 +146,14 @@ func handleConnection(conn net.Conn) error {
 			}
 		case ECHO:
 			if err = handleEcho(conn, args[1]); err != nil {
+				return err
+			}
+		case GET:
+			if err = handleGet(conn, args[1]); err != nil {
+				return err
+			}
+		case SET:
+			if err = handleSet(conn, args[1], args[2]); err != nil {
 				return err
 			}
 		}
